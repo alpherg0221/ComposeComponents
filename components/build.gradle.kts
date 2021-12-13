67d1693a421751a8c950c69643877280f3fe4ceb
alpherg0221 <alpherg0221@gmail.com>
2021-12-13 20:37:14 +0900

    change version

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -87,7 +87,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.alpherg0221"
                 artifactId = "ComposeComponents"
-                version = "1.0.0"
+                version = "1.0.0-alpha01"
             }
         }
     }
