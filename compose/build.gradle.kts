19f20e4d40badb355ffb141bc7c49968d216a7ff
alpherg0221 <78247593+alpherg0221@users.noreply.github.com>
2022-11-16 00:50:14 +0900

    Update libraries

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -12,12 +12,12 @@ plugins {
 group = "com.github.alpherg0221"
 
 android {
-    compileSdk = 32
+    compileSdk = 33
     buildToolsVersion = "31.0.0"
 
     defaultConfig {
         minSdk = 28
-        targetSdk = 32
+        targetSdk = 33
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -59,22 +59,22 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
 
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.activity:activity-ktx:1.5.1")
-    implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.activity:activity-ktx:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.7.0")
 
     // compose
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    implementation("androidx.compose.ui:ui-text-google-fonts:1.3.0-alpha02")
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.4.0-alpha02")
     implementation("androidx.compose.ui:ui-util:$composeVersion")
     implementation("androidx.compose.foundation:foundation:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.material:material-icons-core:$composeVersion")
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.0.0-alpha15")
-    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.compose.material3:material3:1.1.0-alpha02")
+    implementation("androidx.activity:activity-compose:1.6.1")
 
     // accompanist
     implementation("com.google.accompanist:accompanist-coil:0.15.0")
@@ -82,8 +82,8 @@ dependencies {
 
     // test
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
 }
 
@@ -94,7 +94,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.alpherg0221"
                 artifactId = "ComposeComponents"
-                version = "1.0.0-alpha01"
+                version = "1.0.0-alpha09"
             }
         }
     }
