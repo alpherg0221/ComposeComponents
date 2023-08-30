ba58dc9c4a292ca16a41b1a67472cf95c04c6a20
alpherg0221 <78247593+alpherg0221@users.noreply.github.com>
2023-08-30 23:53:43 +0900

    Update libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,7 @@
-buildscript {
-    // Library version
-    val kotlinVersion by extra("1.7.0")
-    val coroutinesVersion by extra("1.6.4")
-    val composeVersion by extra("1.3.0-alpha02")
-    val accompanistVersion by extra("0.26.0-alpha")
-
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    }
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
 task<Delete>("clean") {
--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -1,19 +1,14 @@
-val kotlinVersion: String by project
-val coroutinesVersion: String by project
-val composeVersion: String by project
-val accompanistVersion: String by project
-
 plugins {
     id("com.android.library")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     id("maven-publish")
 }
 
 group = "com.github.alpherg0221"
 
 android {
+    namespace = "jp.gr.java_conf.alpherg0221.compose"
     compileSdk = 33
-    buildToolsVersion = "31.0.0"
 
     defaultConfig {
         minSdk = 28
@@ -51,40 +46,36 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.2.0"
+        kotlinCompilerExtensionVersion = "1.4.3"
     }
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.activity:activity-ktx:1.6.1")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
 
     // compose
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    implementation("androidx.compose.ui:ui-text-google-fonts:1.4.0-alpha02")
-    implementation("androidx.compose.ui:ui-util:$composeVersion")
-    implementation("androidx.compose.foundation:foundation:$composeVersion")
-    implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.compose.material:material-icons-core:$composeVersion")
-    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha02")
-    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.compose.ui:ui:1.5.0")
+    implementation("androidx.compose.ui:ui-tooling:1.5.0")
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.6.0-alpha04")
+    implementation("androidx.compose.ui:ui-util:1.5.0")
+    implementation("androidx.compose.foundation:foundation:1.5.0")
+    implementation("androidx.compose.material:material:1.5.0")
+    implementation("androidx.compose.material:material-icons-core:1.5.0")
+    implementation("androidx.compose.material:material-icons-extended:1.5.0")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha06")
+    implementation("androidx.activity:activity-compose:1.7.2")
 
     // accompanist
     implementation("com.google.accompanist:accompanist-coil:0.15.0")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
 
     // test
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
 }
 
 afterEvaluate {
@@ -94,7 +85,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.alpherg0221"
                 artifactId = "ComposeComponents"
-                version = "1.0.0-alpha09"
+                version = "1.0.0-alpha10"
             }
         }
     }
